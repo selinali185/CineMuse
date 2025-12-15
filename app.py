e7462e7217f69bc83e802a6ae5ab413ffb903eff
selinali185 <selina.li185@gmail.com>
2025-12-15 10:44:10 -0500

    initialized the templates folder

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return "Welcome to my Flask Web Application!"
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug= True)
